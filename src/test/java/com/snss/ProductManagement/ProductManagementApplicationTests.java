7fe0538ca2645947f1e1f6edf519b6204505a01c
Satheeshkumar S <snsatheeshit@gmail.com>
2024-08-04 15:47:02 +0530

    Inital commit

---
+++ src/test/java/com/snss/ProductManagement/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snss.ProductManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
